d62243a3dbebb1faaac83acf6ff585ab728dcb31
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-11 00:29:24 +0900

    banner task started

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
 import About from "./Pages/About/About/About";
+import Navbar from "./Pages/Shared/Navbar/Navbar";
+import Login from "./Pages/Login/Login/Login";
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
