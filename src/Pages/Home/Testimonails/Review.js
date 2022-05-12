965935b133b47055524332d459cd7e11b22c397e
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-12 21:14:55 +0900

    home page is done

--- src/Pages/Home/Testimonails/Review.js
+++ src/Pages/Home/Testimonails/Review.js
@@ -2,21 +2,21 @@ import React from "react";
 
 const Review = ({ review }) => {
   return (
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <div class="card-body">
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <div className="card-body">
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
           voluptatum aut sint voluptas ullam mollitia.
         </p>
-        <div class="flex items-center ">
-          <div class="avatar">
-            <div class="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
+        <div className="flex items-center ">
+          <div className="avatar">
+            <div className="w-16 rounded-full ring ring-primary ring-offset-base-100 mr-5">
               <img src={review.img} alt="" />
             </div>
           </div>
           <div>
-              <h4 className="text-xl">{review.name}</h4>
-              <p>{review.location}</p>
+            <h4 className="text-xl">{review.name}</h4>
+            <p>{review.location}</p>
           </div>
         </div>
       </div>
