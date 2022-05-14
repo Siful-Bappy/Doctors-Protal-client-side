22b696d3341f9200a03234822ff8582a0118e92c
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-14 14:58:06 +0900

    set the login file is done

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from "../../../firebase.init";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is login section</h1>
-            <div>
-            
-            </div>
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if(user) {
+        console.log(user);
+    }
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="text-center font-2xl font-bold">Login</h2>
+          
+          <div class="divider">OR</div>
+          <button onClick={() => signInWithGoogle()} class="btn btn-outline">Continue with Google</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Login;
