b8cb7affb906f6847ddc84f883ce23c24bebd59d
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-18 07:19:12 +0900

    treatment dashboard is on process

--- src/Pages/Dashboard/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointment.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../firebase.init";
 
 const MyAppointment = () => {
-    return (
-        <div>
-            <h2>My Appiontments</h2>
-        </div>
-    );
+  const [user, loading, error] = useAuthState(auth);
+
+  const [appointments, setAppointments] = useState([]);
+  useEffect(() => {
+    if (user) {
+      fetch(`http://localhost:5000/booking?patient=${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setAppointments(data));
+    }
+  }, [user]);
+  return (
+    <div>
+      <h2>My Appiontments: {appointments.length}</h2>
+      <div class="overflow-x-auto">
+  <table class="table w-full">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Treatment</th>
+      </tr>
+    </thead>
+    <tbody>
+        {
+            appointments.map(a => <tr>
+                <th>1</th>
+                <td>{a.patient}</td>
+                <td>{a.date}</td>
+                <td>{a.slot}</td>
+                <td>{a.treatment}</td>
+              </tr>)
+        }
+    </tbody>
+  </table>
+</div>
+    </div>
+  );
 };
 
 export default MyAppointment;
