ff2b1187c90c9efeabdc4bbae77c0113c7591a91
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-18 06:30:48 +0900

    dashboard is done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import SignUp from "./Pages/Login/Login/SignUp";
 import RequireAuth from "./Pages/Login/Login/RequireAuth";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import MyAppointment from "./Pages/Dashboard/MyAppointment";
+import MyReview from "./Pages/Dashboard/MyReview";
 
 function App() {
   return (
@@ -25,6 +28,14 @@ function App() {
             <Appointment />
           </RequireAuth>
         } />
+        <Route path="dashboard" element={
+          <RequireAuth>
+            <Dashboard />
+          </RequireAuth>
+        }>
+          <Route index element={<MyAppointment></MyAppointment>}></Route>
+          <Route path="review" element={<MyReview></MyReview>}></Route>
+        </Route>
       </Routes>
       <ToastContainer />
     </div>
